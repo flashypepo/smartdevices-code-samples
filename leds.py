b3c5d71f9858e20effe33c7d190663c4f128207a
PePo <peter@pepo.nl>
2018-09-06 13:55:00 +0200

    Setup for classroom - college 1. deleted files: they come back in later colleges.

--- leds.py
+++
@@ -1,6 +0,0 @@
-# digital IO
-# blinking leds
-# 3 LEDS connected to LoPy4
-# see Jupyter notebook 'LoPy4 demos - digital_IO'
-# 2018-0817 Peter
-print("See 'Smart Devices - LoPy4 demos - digital_IO'"")
--- main.py
+++ main.py
@@ -10,10 +10,7 @@ import micropython
 micropython.alloc_emergency_exception_buf(100)
 
 # used devices in main:
-use_oled_spi = True # OLED - SPI (default)
-use_oled_i2c = True # OLED - I2C
-use_bme280 = True   # weatherstation demo
-use_gfx = True      # graphics demo
+# Peter: komt later
 
 # #################################
 # Wifi Windesheim network
@@ -21,11 +18,9 @@ use_gfx = True      # graphics demo
 import wifimanager
 
 print('Creating wifi object with a wificonfig-json ...')
-wifi = wifimanager.WifiManager('config/wificonfig_home.json') # HOME config
-#TODO: wifi = wifimanager.WifiManager('wificonfig.json') # SAMPLE Wifi-WF config
-#print('EXERCISE: include connecting to Wifi...')
+wifi = wifimanager.WifiManager('wificonfig.json') # SAMPLE Wifi-WF config
 
-#''' TODO: include connecting to Wifi
+''' TODO: include connecting to Wifi
 print('Connecting to network ...')
 wifi.connect()
 print('Device IP: {0}'.format(wifi._wlan.ifconfig()[0])) # device IP
@@ -45,47 +40,3 @@ print('TODO: EXERCISE: include connecting to Wifi...')
 # show MAC-address
 # #################################
 print('MAC-adres:', wifi.mac)
-
-# #################################
-# Experment: OLED 128*32 SPI and I2C
-# 2018-0821 added OLED-I2C
-# 2018-0819 okay with ssd1306 of DiCola
-# #################################
-if use_oled_spi:
-    from test_oled_spi import demo
-    demo(wifi.mac)
-
-if use_oled_i2c:
-    from test_oled_i2c import demo
-    demo(wifi.mac)
-
-if use_gfx and use_oled_spi:
-    from test_oled_spi import oled as display
-    from test_gfx import demo
-    demo(display)
-
-if use_gfx and use_oled_i2c:
-    from test_oled_i2c import oled as display
-    from test_gfx import demo
-    demo(display)
-
-
-# #################################
-# Experment: BME280, I2C temperature sensor
-# 2018-0821: requires i2c from test_oled_i2c
-# TODO WISH: separate classes for I2C_BUS and SPI_BUS objects
-# #################################
-if use_bme280:
-    from test_oled_i2c import i2c
-
-    # Select display either I2C or SPI...
-    # Toggle '#' on next line
-    ''' use OLED-I2C display
-    from test_oled_i2c import oled as display
-    '''
-    #use OLED-SPI display
-    from test_oled_spi import oled as display
-    #'''
-
-    import test_bme280
-    test_bme280.demo(i2c, display, 2)
