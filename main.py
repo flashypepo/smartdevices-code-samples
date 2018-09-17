3e2e6ef145fd82cc1a92a47e34319f9615599385
PePo <peter@pepo.nl>
2018-09-17 10:58:30 +0200

    added place-holders for all demo modules.

--- main.py
+++ main.py
@@ -12,11 +12,29 @@ micropython.alloc_emergency_exception_buf(100)
 
 # used devices in main:
 
-# TODO: College 2: digital IO, analog IO
-use_leds = True  # demo 3-LEDs G28/P12, G22/P11, G17/P10
-use_button = True # demo button G10/P23
-use_ldr = True # demo analog LDR sensor G13/P16
-use_tmp36 = True # demo analog TMP36 sensor G0/P15
+# College 2: digital IO, analog IO
+use_leds = False  # demo 3-LEDs G28/P12, G22/P11, G17/P10
+use_button = False # demo button G10/P23
+use_ldr = False # demo analog LDR sensor G13/P16
+use_tmp36 = False # demo analog TMP36 sensor G0/P15
+
+# TODO: College 3: SPI (OLED display), PWM (Servo, LED), I2C (example sensor?)
+use_spi_oled = False  # demo OLED-SPI display (default)
+use_gfx = False       # EXTRA demo for drawing graphics on OLED-display(s)
+
+use_pwm_servo = False # demo PWM to control a servo
+use_pwm_led = False   # demo of PWM to control intensity of LED
+
+use_bme280 = False    # demo digital temperature sensor, sensor not in electronic package
+use_i2c_oled = False  # demo OLED - I2C, display not in electronic package
+
+
+# TODO: College 4: Wifi, MQTT?
+use_wifi = False   # demo Wifi
+use_mqtt = False   # demo MQTT
+
+# TODO: College 5, 6: LoRa / LoRaWAN
+use_lorawan = False
 
 
 # #################################
@@ -24,24 +42,24 @@ use_tmp36 = True # demo analog TMP36 sensor G0/P15
 # #################################
 import wifimanager
 
-print('Creating wifi object with a wificonfig-json ...')
+#print('Creating wifi object with a wificonfig-json ...')
 wifi = wifimanager.WifiManager('config/wificonfig_template.json') # TEMPLATE Wifi-WF config
 
-''' TODO: include connecting to Wifi
-print('Connecting to network ...')
-wifi.connect()
-print('Device IP: {0}'.format(wifi._wlan.ifconfig()[0])) # device IP
+#''' TODO: College 4 include connecting to Wifi
+#print('Wifi: connecting to a Wifi network...', use_wifi)
+if use_wifi:
+    print('Connecting to network ...')
+    wifi.connect()
+    print('Device IP: {0}'.format(wifi._wlan.ifconfig()[0])) # device IP
 
-
-# #################################
-# change password for Telnet, FTP etc.
-# #################################
-print('Updating username and password for telnet/ftp...')
-wifi.change_access('micro', 'python') #change if you want
+    # #################################
+    # change password for Telnet, FTP etc.
+    # #################################
+    print('Updating username and password for telnet/ftp...')
+    wifi.change_access('micro', 'python') #change if you want
 
 #END_OF_TODO '''
 
-print('TODO: EXERCISE: include connecting to Wifi...')
 
 # #################################
 # show MAC-address
@@ -110,3 +128,29 @@ if use_ldr:
 
     except KeyboardInterrupt:
         print('LDR demo done')
+
+
+# #################################
+# College 3- M2M protocols SPI, I2C, PWM
+# 2018-0921 Peter new
+# #################################
+
+print('M2M protocol: SPI OLED-display ...', use_spi_oled)
+print('M2M protocol: PWM (servo) ...', use_pwm_servo)
+print('M2M protocol: PWM (fading lED) ...', use_pwm_led)
+
+print('M2M protocol: I2C OLED-display ...', use_i2c_oled)
+print('M2M protocol: I2C BME280 ...', use_bme280)
+
+# #################################
+# College 4 - communications: Wifi, MQTT
+# 2018-0921 Peter new
+# #################################
+print('Communications: Wifi ...', use_wifi)
+print('Communications: MQTT ...', use_mqtt)
+
+# #################################
+# College 5, 6 - communications: LoRa
+# 2018-0921 Peter new
+# #################################
+print('Communications: LoRa ...', use_lorawan)
